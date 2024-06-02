e41d1e53386ecc7a10f0107fb20784a330da433b
Zachary Gibson <zackgib@aol.com>
2024-06-02 10:50:47 -0500

    Initial Commit

---
+++ TCA GRDB Playground/TCA_GRDB_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_GRDB_PlaygroundApp.swift
+//  TCA GRDB Playground
+//
+//  Created by Zachary Gibson on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_GRDB_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
